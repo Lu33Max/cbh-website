8d35bd83beb03db38a6cae7b590d3b0692040e96
Vulpox <117985228+Vulpox@users.noreply.github.com>
2023-05-23 17:00:57 +0200

    service and navigation
    layout

--- src/components/overall/headerNEW.tsx
+++ src/components/overall/headerNEW.tsx
@@ -21,19 +21,29 @@ const HeaderNEW: React.FC = () => {
     void router.push(`/search?q=${encodedSearchQuery}`);
   }
 
+  const navigationButtons = [
+    { name: 'Home', link: '/' },
+    { name: 'Explore', link: '/search' },
+    { name: 'Expert Search', link: '/expertsearch' },
+    { name: 'Cart', link: '/Cart' },
+    { name: 'About Us', link: '/About' },
+    { name: 'Become a Supplier', link: '/BecomeSup' },
+    
+  ];
+
   return (
     <>
       <div className='bg-white h-[80px] flex flex-row pl-10 pr-5 items-center font-body font-poppins text-xl text-[#164A41] font-thin'>
         <Image src="/cbh_logos/logo_black.png" alt="Logo" width={200} height={80} className='z-20'/>
         <div className='flex flex-row justify-between mx-auto gap-5 z-20'>
           <button onClick={() => {activeHeader === 1 ? setActiveHeader(0) : setActiveHeader(1)}} className='flex flex-row'>Services
-
+          
           <svg width="12" height="21" viewBox="0 0 20 36" fill="none" xmlns="http://www.w3.org/2000/svg" className="transform rotate-90 translate-y-[4px] ml-2  " >
             <path opacity="0.4" d="M13.2156 9.00221L0 18.6931L0 33.0375C0 35.4922 3.03565 36.7195 4.81522 34.9808L18.371 21.7359C20.543 19.6136 20.543 16.1617 18.371 14.0394L13.2156 9.00221Z" fill="black"/>
             <path d="M0 2.76626V18.6961L13.2156 9.00524L4.81522 0.797406C3.03565 -0.915755 0 0.311585 0 2.76626Z" fill="black"/>
-            </svg>
+          </svg>
 
-             </button>
+          </button>
 
           <button onClick={() => {activeHeader === 2 ? setActiveHeader(0) : setActiveHeader(2)}} className='flex flex-row'>Navigation
 
@@ -75,11 +85,67 @@ const HeaderNEW: React.FC = () => {
           </button>
         */}
       </div>
-      <div className={`absolute top-0 w-full text-black bg-white z-10 ${activeHeader === 1 ? "h-[400px]" : "h-0 hidden" }`}>
-        Header1
+
+      {/* Services */}
+      <div className={`absolute top-0 w-full text-black bg-gray-300 z-10 ${activeHeader === 1 ? "h-[450px]" : "h-0 hidden" }`}>
+        <div className="flex flex-row w-full items-center justify-center mt-[5%]">
+          <div className="flex flex-col items-center border border-solid border-black mx-3">
+            <label className="mb-2">Label</label>
+            <Image src="/CBH Logo.png" alt="Logo" width={200} height={200} className="mx-10" />
+          </div>
+          <div className="flex flex-col items-center border border-solid border-black mx-3">
+            <label className="mb-2">Label</label>
+            <Image src="/CBH Logo.png" alt="Logo" width={200} height={200} className="mx-10" />
+          </div>
+          <div className="flex flex-col items-center border border-solid border-black mx-3">
+            <label className="mb-2">Label</label>
+            <Image src="/CBH Logo.png" alt="Logo" width={200} height={200} className="mx-10" />
+          </div>
+        </div>
+        <div className="flex flex-row w-full items-center justify-center absolute bottom-0 left-0 bg-white h-[15%]">
+          <div className="flex flex-col items-start mx-3">
+            <label style={{ fontSize: '18px' }}>Label</label>
+          </div>
+          <div className="flex flex-col items-center mx-3">
+            <button className="bg-gray-300" style={{ fontSize: '18px' }}>Button</button>
+          </div>
+          <div className="flex flex-col items-end mx-3">
+            <label style={{ fontSize: '18px' }}>Label</label>
+          </div>
+          <div className="flex flex-col items-center mx-3">
+            <button className="bg-gray-300" style={{ fontSize: '18px' }}>Button</button>
+          </div>
+        </div>
       </div>
-      <div className={`absolute top-0 w-full text-black bg-white z-10 ${activeHeader === 2 ? "h-[400px]" : "h-0 hidden" }`}>
-        Header2
+
+      {/* Navigation */}
+      <div className={`absolute top-0 w-full text-black bg-gray-300 z-10 ${activeHeader === 2 ? "h-[450px]" : "h-0 hidden" }`}>
+        <div className="flex flex-col items-center mt-[2%]">
+          <nav style={{ display: "flex", flexDirection: "column", justifyContent: "center" }} className="py-5 items-center">
+            {navigationButtons.map((link, index) => (
+              <Link key={index} href={link.link}> 
+                <div className={`relative block w-full h-full py-2 px-4 font-medium text-gray-700 hover:bg-gray-100 transition duration-300 ease-in-out button-sidebar ${router.pathname === link.link ? 'bg-gray-200' : ''}`}>
+                  <div className={`activeButton absolute h-full w-[15%] bg-emerald-700 left-[-20%] ${router.pathname === link.link ? '' : 'hidden'}`}></div>
+                  {link.name}                                 
+                </div>
+              </Link>
+            ))}
+          </nav>
+        </div>
+        <div className="flex flex-row w-full items-center justify-center absolute bottom-0 left-0 bg-white h-[15%]">
+          <div className="flex flex-col items-start mx-3">
+            <label style={{ fontSize: '18px' }}>Label</label>
+          </div>
+          <div className="flex flex-col items-center mx-3">
+            <button className="bg-gray-300" style={{ fontSize: '18px' }}>Button</button>
+          </div>
+          <div className="flex flex-col items-end mx-3">
+            <label style={{ fontSize: '18px' }}>Label</label>
+          </div>
+          <div className="flex flex-col items-center mx-3">
+            <button className="bg-gray-300" style={{ fontSize: '18px' }}>Button</button>
+          </div>
+        </div>
       </div>
     </>
   )
